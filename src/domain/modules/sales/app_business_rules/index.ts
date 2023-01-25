c433427b2b0b1378b21bff9ab813ae69a8888170
juang-10 <judagosa8520@gmail.com>
2023-01-25 16:07:52 -0500

    feat: Add update one and delete one to the index

--- src/domain/modules/sales/app_business_rules/index.ts
+++ src/domain/modules/sales/app_business_rules/index.ts
@@ -2,6 +2,8 @@ import { SalesRepository } from "../../sales/interface_adapters/repositories/sal
 import { SalesPsqlRepository } from '../../../../framework_and_drivers/storage/postgres/repositories/sales/sales_psql_repo'
 import { buildCreateOneSale } from "./use_cases/create_one";
 import { buildCountList, buildSalesList } from './use_cases/get_all'
+import { buildDeleteOneSale } from "./use_cases/delete_one";
+import { buildUpdateSales } from "./use_cases/update_one";
 
 const salesRepository: SalesRepository = new SalesRepository(
     new SalesPsqlRepository()
@@ -10,16 +12,22 @@ const salesRepository: SalesRepository = new SalesRepository(
 const createOne = buildCreateOneSale(salesRepository);
 const getList = buildSalesList(salesRepository);
 const countList = buildCountList(salesRepository);
+const deleteOne = buildDeleteOneSale(salesRepository);
+const updateOne = buildUpdateSales(salesRepository);
 
 const service = {
     createOne,
     getList,
-    countList
+    countList,
+    deleteOne,
+    updateOne
 }
 
 export default service;
 export {
 	createOne,
     getList,
-    countList
+    countList,
+    deleteOne,
+    updateOne
 }
