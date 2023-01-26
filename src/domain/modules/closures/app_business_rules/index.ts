56e7caf68b5e0b77e8e19dcfd0ba8285239a22e6
juang-10 <judagosa8520@gmail.com>
2023-01-26 17:27:32 -0500

    add: buildClosureList

--- src/domain/modules/closures/app_business_rules/index.ts
+++ src/domain/modules/closures/app_business_rules/index.ts
@@ -1,25 +1,21 @@
-import { ClosuresRepository } from "../interface_adapters/repositories/closures_repo";
-import { ClosuresPsqlRepository } from "../../../../framework_and_drivers/storage/postgres/repositories/closures/closures_psql_repo";
-import { buildCreateOneClosure } from "./uses_cases/create_one";
-import { buildClosuresList, buildCountList } from "./uses_cases/get_all";
+import { ClosuresRepository } from '../interface_adapters/repositories/closures_repo';
+import { ClosuresPsqlRepository } from '../../../../framework_and_drivers/storage/postgres/repositories/closures/closures_psql_repo';
+import { buildCreateOneClosure } from './uses_cases/create_one';
+import { buildClosuresList, buildCountList } from './uses_cases/get_all';
 
 const closuresRepository: ClosuresRepository = new ClosuresRepository(
-    new ClosuresPsqlRepository()
-)
+  new ClosuresPsqlRepository()
+);
 
-const createOne = buildCreateOneClosure(closuresRepository);
 const getList = buildClosuresList(closuresRepository);
+const createOne = buildCreateOneClosure(closuresRepository);
 const countList = buildCountList(closuresRepository);
 
 const service = {
-    createOne,
-    getList,
-    countList
-}
+  getList,
+  createOne,
+  countList,
+};
 
 export default service;
-export {
-    createOne,
-    getList,
-    countList
-}
+export { getList, createOne, countList };
