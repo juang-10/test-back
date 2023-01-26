883a664a07b3e1f6f870cd15d2762396da9f88ed
juang-10 <judagosa8520@gmail.com>
2023-01-26 09:41:55 -0500

    add: closures routes

--- src/framework_and_drivers/web/routes/index.ts
+++ src/framework_and_drivers/web/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { router as mainRouter } from './main';
 import { router as productsRouter } from './products/products_routes';
 import { router as salesRouter } from './sales/sales_routes';
+import { router as closuresRouter } from './closures/closures_routes';
 
 const routes = Router();
 routes.use('/', mainRouter);
 routes.use('/api/v1/products', productsRouter);
 routes.use('/api/v1/sales', salesRouter);
+routes.use('/api/v1/closures', closuresRouter);
+
 
 export { routes };
