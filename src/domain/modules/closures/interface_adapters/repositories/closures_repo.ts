746d9029b1b682beac73b86cc1d0c0523958f571
juang-10 <judagosa8520@gmail.com>
2023-01-26 09:40:30 -0500

    add: ClosuresRepository

---
+++ src/domain/modules/closures/interface_adapters/repositories/closures_repo.ts
@@ -0,0 +1,39 @@
+import { IRead, IWrite } from "../../../../common/interfaces/ioperations";
+
+
+export class ClosuresRepository implements IRead, IWrite {
+
+    repository: any;
+
+    constructor(repository: any) {
+        this.repository = repository;
+    }
+
+    async getAllItems(opts: any, transaction?: any) {
+        return await this.repository.getAllItems(opts, transaction);
+    }
+
+    async getItem(searchCriter: any, transaction?: any) {
+        return await this.repository.getItem(searchCriter, transaction);
+    }
+
+    async createItem(item: any, transaction?: any) {
+        return await this.repository.createItem(item, transaction);
+    }
+
+    async updateItem(id: string, item: any, transaction?: any) {
+        return await this.repository.updateItem(id, item, transaction);
+    }
+
+    async deleteItem(deleteCriter: any, transaction?: any) {
+        return await this.repository.deleteItem(deleteCriter, transaction);
+    }
+
+    async countItems(filter: any, transaction?: any) {
+        return await this.repository.countItems(filter, transaction);
+    }
+
+    async getNewTransaction(autocommit: boolean, parentTransaction?: any) {
+        return await this.repository.getNewTransaction(autocommit, parentTransaction);
+    }
+}
