3cdf4f172e07414e0e63e8036637c294c5864f49
juang-10 <judagosa8520@gmail.com>
2023-01-26 10:12:09 -0500

    add: buildClosuresList and buildCountList

--- src/domain/modules/closures/app_business_rules/index.ts
+++ src/domain/modules/closures/app_business_rules/index.ts
@@ -1,25 +1,25 @@
 import { ClosuresRepository } from "../interface_adapters/repositories/closures_repo";
 import { ClosuresPsqlRepository } from "../../../../framework_and_drivers/storage/postgres/repositories/closures/closures_psql_repo";
 import { buildCreateOneClosure } from "./uses_cases/create_one";
-// import { buildCountList, buildProductsList } from "./use_cases/get_all";
+import { buildClosuresList, buildCountList } from "./uses_cases/get_all";
 
 const closuresRepository: ClosuresRepository = new ClosuresRepository(
     new ClosuresPsqlRepository()
 )
 
 const createOne = buildCreateOneClosure(closuresRepository);
-// const getList = buildProductsList(closuresRepository);
-// const countList = buildCountList(closuresRepository);
+const getList = buildClosuresList(closuresRepository);
+const countList = buildCountList(closuresRepository);
 
 const service = {
     createOne,
-    // getList,
-    // countList
+    getList,
+    countList
 }
 
 export default service;
 export {
     createOne,
-    // getList,
-    // countList
+    getList,
+    countList
 }
